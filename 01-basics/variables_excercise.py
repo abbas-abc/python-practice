48ed7d9bd9ad7ddb3cca6c010ede92deabf8f4bb
abbas-abc <afaq.abbas512@gmail.com>
2026-08-25 17:24:58 +0500

    Add variables exercise creating the profile card

---
+++ 01-basics/variables_excercise.py
@@ -0,0 +1,7 @@
+name = "Afaq"
+age = 18   
+city = "Islamabad"
+language = "python"
+
+print("My name is " + name + " and i am " + str(age) + " years old.")
+print("I am living in " + city + " and nowadays learning " + language + ".")
