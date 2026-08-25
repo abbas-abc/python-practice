56b440da867ebc92dc7eba83ef75155d55662372
abbas-abc <afaq.abbas512@gmail.com>
2026-08-25 16:11:14 +0500

    Add first hello world program

--- 01-basics/hello.py
+++ 01-basics/hello.py
@@ -1,1 +1,4 @@
-print("hello world")
+print("hello world,")
+print("My name is Muhammad Afaq Abbas.")
+print("My favorite hobby is coding.")
+print("I am learning python because, i want to become AI security engineer in future.")
