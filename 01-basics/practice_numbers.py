50a2d655de26054b6a09967204fe5a460ee8bb12
abbas-abc <afaq.abbas512@gmail.com>
2026-08-26 15:50:09 +0500

    Add task 4: formatted equation output using print and sep

--- 01-basics/practice_numbers.py
+++ 01-basics/practice_numbers.py
@@ -1,4 +1,14 @@
+# Task 3: Ask user for two numbers and print their sum
 num1 = int(input("First number: "))
 num2 = int(input("Second number: "))
 total = num1 + num2
 print("Sum: ",total)
+
+
+
+#Task 4: formatted equation using sep
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+total = num1 + num2
+print(num1, "+", num2, "=", total, sep=" ")  #used sep parameter to give all equal space
+
