fd3d3008da75531a3841b66ed9434450e35ad34d
abbas-abc <afaq.abbas512@gmail.com>
2026-08-27 13:10:03 +0500

    Add tip calculator challenge using float, int, and percentage math

---
+++ 01-basics/tip_calculator.py
@@ -0,0 +1,16 @@
+#tip calculator 
+
+Total_bill = float(input("Enter total bill amount = " ))     #used float becuase it can contain decimal
+peoples = int(input("Enter total number of peoples = "))     #int is used becuase the number of people cannot be in decimal
+tip_percentage = int(input("enter tip percentage = "))
+tip_amount= Total_bill*tip_percentage/100           #covert percentage into amount
+
+print("Each person owns = ", Total_bill/peoples)
+
+Total_with_tip = Total_bill+tip_amount
+
+print("Total amount with tip = ", Total_with_tip)
+
+person_with_tip_own = Total_with_tip/peoples
+
+print("Each person with tip owns = ", person_with_tip_own)
