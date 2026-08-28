66ddc8fe4bcac27848b7084bf3b8e1f1f2fb21c8
abbas-abc <afaq.abbas512@gmail.com>
2026-08-28 15:38:54 +0500

    Adds practice functions

---
+++ 01-basics/practice_functions.py
@@ -0,0 +1,11 @@
+
+def greet_user(name):           #through dep() we can make custom functions
+    print("Welcome,", name+"!" " Let's learn Python.")
+greet_user("Afaq")         #we can reuse that functions with writng it over and over
+greet_user("Abbas")    
+
+
+def cube(number):
+    return number**3
+result = cube(2)
+print("Cube of 2 is", result )
