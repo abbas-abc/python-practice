8aed74ed9b75b30989e0c53ea432c9d500825fb9
abbas-abc <afaq.abbas512@gmail.com>
2026-09-03 16:57:36 +0500

    Add 02-conditionals folder with university eligibility checker

---
+++ 02-conditionals/university_eligibility.py
@@ -0,0 +1,6 @@
+question = input("Have you completed your high school? ")
+if question == "yes":
+    print("You are eligible for taking admission in university.")  #used if and else condtions to check simple eligibility
+
+else:
+    print("You are not eligible for admission in university.")
