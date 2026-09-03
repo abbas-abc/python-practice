f00dc99f27410e5f8cb9efc27e7c382506ec141e
abbas-abc <afaq.abbas512@gmail.com>
2026-09-03 19:36:58 +0500

    Add marking_grades.py with if-elif-else practice

---
+++ 02-conditionals/marking_grades.py
@@ -0,0 +1,13 @@
+percentage = int(input("How many percentage you got in Math? " ))
+if percentage >= 80:
+    print("You got A grade.")  #we add 4 conditions to give the grade according to the percentage in the subject 
+elif percentage >= 70:
+    print("You got B grade")
+elif percentage >= 60:
+    print("You got C grade")
+elif percentage >= 50:
+    print("You got C grade")
+
+else:
+    print("You fail this subject.")
+
---
+++ conditonals.py
@@ -0,0 +1,6 @@
+question = input("Have you completed your high school? ")
+if question == "yes":
+    print("You are eligible for taking admission in university")  #used if and else condtions
+
+else:
+    print("You are not eligible for admission in university")
