4b9879c5c7de535138bd43ccc54954dabcfb50b7
Muhammad Afaq Abbas <afaq.abbas512@gmail.com>
2026-09-04 06:29:56 +0500

    Delete 02-conditionals/login_page.py

--- 02-conditionals/login_page.py
+++
@@ -1,7 +0,0 @@
-user_name = input("username: ")
-password = input("password: ")
-
-if user_name == "admin" and password == "pass":
-    print("You are successfully login")                 #we use and with addition to if and else
-else:
-    print("Your username or password is incorrect.")
