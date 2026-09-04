3571ccae2d5743a23f7abab25adfe82d42a993a0
abbas-abc <afaq.abbas512@gmail.com>
2026-09-04 06:47:30 +0500

    Add multiple_checker.py to check if a number is a multiple of another

---
+++ 02-conditionals/multiple_checker.py
@@ -0,0 +1,5 @@
+number = int(input("Enter any number? "))
+if (number % 5 == 0):                       #when the remainder will equal to zero so it is multiple of that number
+    print(number, "is multiple of 5." )
+else:
+    print(number, "is not multiple of 5.")
