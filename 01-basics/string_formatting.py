bde430bcfca4fe8367909171e0415c8c3f99dac5
abbas-abc <afaq.abbas512@gmail.com>
2026-09-06 22:24:55 +0500

    Add string_formatting.py practicing .format() with positional, indexed, and named arguments

---
+++ 01-basics/string_formatting.py
@@ -0,0 +1,14 @@
+a=6
+b=7
+sum=a + b
+
+print("sum is {}".format(sum))
+print("language is{}".format(" python"))
+
+print("sum of {} and {} is {}".format(a,b,sum))
+
+#index based formatting
+print("sum of {1} and {0} is {2}".format(a,b,sum))
+
+#value base formatting
+print("values of a is {a} and b is {b}".format(a=5,b=7))
