b39b8c92f7213d0b9823d27b6529c30df9a0c238
abbas-abc <afaq.abbas512@gmail.com>
2026-09-08 16:47:02 +0500

    Add tuple_methods.py practicing looping, sum accumulator, and .index()/.count() on tuples

---
+++ 05-data-structures/tuple_methods.py
@@ -0,0 +1,15 @@
+tup=(2,4,6,31,2,6,4,3,4,4,3)
+
+#looping
+#sum of values through looping in tuples
+sum=0
+for val in tup:
+    sum+=val
+print(sum)    
+
+
+#methods
+print(tup.index(2))  #the .index gives the first occurrence index
+
+print(tup.count(4)) #show that how many times the number repeated
+
