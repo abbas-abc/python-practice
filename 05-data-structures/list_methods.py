2f1aaea532c121a2fae86ab11cb97456f96d5c1a
abbas-abc <afaq.abbas512@gmail.com>
2026-09-08 12:43:03 +0500

    Add 05-data-structures folder with list_methods.py

---
+++ 05-data-structures/list_methods.py
@@ -0,0 +1,23 @@
+nums = [1,6,5,3,4]
+
+# This will add the number at the last of the list
+nums.append(10)
+print(nums)
+
+#it insert the specific value of specific index number
+nums.insert(2,11)
+print(nums)
+
+# It makes the whole list in reverse order
+nums.reverse()
+print(nums)
+
+# It makes the numbers in ascending order
+nums.sort()
+print(nums)
+
+# It makes the numbers in descending order
+nums.sort(reverse=True)
+print(nums)
+
+
