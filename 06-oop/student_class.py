ddfa33530058082282cbf2b858e0d6d7005fff9b
abbas-abc <afaq.abbas512@gmail.com>
2026-09-15 15:18:03 +0500

    Add student_class.py practicing __init__ constructor with multiple objects

---
+++ 06-oop/student_class.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self,name,cgpa):
+        self.name=name
+        self.cgpa=cgpa
+stud1=student("Afaq",4.0)
+stud2=student("Abbas",3.5)
+stud3=student("Khan",2.8)
+
+print(stud1.name)
+print(stud2.cgpa)
+print(stud3.name)
