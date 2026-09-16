02d85e3b8d64ab8e93ba6b67beb0e8be2a8e3613
abbas-abc <afaq.abbas512@gmail.com>
2026-09-16 08:30:49 +0500

    Update bank_account.py: add setter method

--- 06-oop/bank_account.py
+++ 06-oop/bank_account.py
@@ -5,6 +5,11 @@ class BankAccount:
     def get_balance(self):
         return self.__balance 
     
+    def set_balance(self,newBalance):
+        self.__balance=newBalance
+        
+    
 a1= BankAccount("Afaq",1000)
+a1.set_balance(20_000)
 print(a1.name, a1.get_balance())
 
